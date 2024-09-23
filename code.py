1c6b53cb435c84fac5c9053456259407bf6ef887
Marpaka Pradeep Sai <153713502+MarpakaPradeepSai@users.noreply.github.com>
2024-09-23 14:51:01 +0530

    Update code.py

--- code.py
+++ code.py
@@ -61,23 +61,23 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # Prediction button
-if st.button('Predict🔎'):
+if st.button('Predict 🔍'):
     if None in [pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree]:
         st.warning('⚠️ Please provide all fields.')
     else:
         result = predict_diabetes(pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree)
         if result == 1:
             st.markdown("""
-                <div style="background-color:red; padding:20px; color:white; text-align:center; border-radius:10px;">
-                    <h3 style="color:black;">🚨 <strong>Warning!</strong></h3>
+                <div style="background-color:red; padding:20px; text-align:center; border-radius:10px;">
+                    <h3 style="color:white;">🚨 <strong>Warning!</strong></h3>
                     <p style="font-size:18px; color:white;">You have a <strong>high risk</strong> of diabetes.</p>
                     <p style="color:white;">It's time to take action! Consult a healthcare professional for advice on lifestyle changes.</p>
                 </div>
             """, unsafe_allow_html=True)
         else:
             st.markdown("""
-                <div style="background-color:green; padding:20px; color:white; text-align:center; border-radius:10px;">
-                    <h3 style="color:black;">✅ <strong>Good News!</strong></h3>
+                <div style="background-color:green; padding:20px; text-align:center; border-radius:10px;">
+                    <h3 style="color:white;">✅ <strong>Good News!</strong></h3>
                     <p style="font-size:18px; color:white;">You have a <strong>low risk</strong> of diabetes.</p>
                     <p style="color:white;">Keep up the healthy habits! Stay active and maintain a balanced diet.</p>
                 </div>
